9d57a65d925ecc02b3084b6528dc89c0b945f0d8
Bilal Dırmık <bigisivri@outlook.com>
2021-09-02 15:32:02 +0300

    New commit.

--- blogSpring/src/main/java/blogSpring/entities/dtos/PostWithCategoryDto.java
+++ blogSpring/src/main/java/blogSpring/entities/dtos/PostWithCategoryDto.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class PostWithCategoryDto {
+public class PostWithCategoryDto { // Inactive Class
 	
 	private int id;
 	
