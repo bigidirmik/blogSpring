9a5b6c568934e0f86f3551d57a6a200b98978155
Bilal Dırmık <bigisivri@outlook.com>
2021-09-09 23:48:41 +0300

    jlog

--- blogSpring/src/main/java/blogSpring/externalServices/jLogger/JLoggerManager.java
+++ blogSpring/src/main/java/blogSpring/externalServices/jLogger/JLoggerManager.java
@@ -2,6 +2,6 @@ package blogSpring.externalServices.jLogger;
 
 public class JLoggerManager {
 	public void log(String message) {
-		System.out.println("J Logger ile loglandı (Sim�lasyon) : " + message);
+		System.out.println("J Logger ile loglandı (Simulasyon) : " + message);
 	}
 }
