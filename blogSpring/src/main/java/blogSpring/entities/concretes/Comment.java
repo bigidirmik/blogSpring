8bc609842fe432eaaa15b201b6d68864e979a758
Bilal Dırmık <bigisivri@outlook.com>
2021-09-09 19:13:56 +0300

    lob

--- blogSpring/src/main/java/blogSpring/entities/concretes/Comment.java
+++ blogSpring/src/main/java/blogSpring/entities/concretes/Comment.java
@@ -6,6 +6,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.Email;
@@ -42,6 +43,7 @@ public class Comment {
 	@NotBlank
 	@NotNull
 	@Column(name = "content", columnDefinition="TEXT", length = 25000)
+	@Lob
 	private String content;
 	
 	//ORM
