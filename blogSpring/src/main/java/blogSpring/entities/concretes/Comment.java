590af4e5e26ca7c04b825827b929df0bec9acdee
Bilal Dırmık <bigisivri@outlook.com>
2021-09-13 18:12:05 +0300

    comment

--- blogSpring/src/main/java/blogSpring/entities/concretes/Comment.java
+++ blogSpring/src/main/java/blogSpring/entities/concretes/Comment.java
@@ -25,7 +25,6 @@ import lombok.NoArgsConstructor;
 @Table(name = "comments")
 public class Comment {
 	
-	@JsonIgnore
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id")
